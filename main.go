9fa7a4c56a816e8c4ec0be778121624400735c07
banuelossoto-leticia <banuelossoto.leticia@gmail.com>
2020-02-22 12:22:22 -0800

    inital commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	// Install with `go get github.com/eclipse/paho.mqtt.golang
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"os"
+	"time"
+	"fmt"
+)
+
+func main() {
+	defaultHandler := func(c mqtt.Client, m mqtt.Message) {
+		fmt.Println(m)
+	}
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker("tcp://bastc.local:1883")
+	opts.SetClientID("go thingy")
+	opts.SetDefaultPublishHandler(defaultHandler)
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	handleQuit := func(c mqtt.Client, m mqtt.Message) {
+		os.Exit(0)
+	}
+
+	if token := client.Subscribe("quit", 0, handleQuit); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	for {
+		fmt.Println("Publishing \"hello\" to \"hello\"")
+
+		if token := client.Publish("hello", 0, false, "hello"); token.Wait() && token.Error() != nil {
+			fmt.Println(token.Error())
+		}
+
+		time.Sleep(time.Second)
+	}
+}
