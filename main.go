89c0f1e57055cc73dc8fb7d68e0658570ac9c890
banuelossoto-leticia <banuelossoto.leticia@gmail.com>
2020-02-29 20:00:15 +0000

    pin pad code was added

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	// Install with `go get github.com/eclipse/paho.mqtt.golang
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"os"
-	"time"
 	"fmt"
+	"github.com/stianeikeland/go-rpio"
 )
 
 func main() {
@@ -31,15 +31,72 @@ func main() {
 		panic(token.Error())
 	}
 
+	if err := rpio.Open(); err != nil {
+		panic(err)
+	}
+	defer rpio.Close()
+
+	//////////For Pin Pad//////////
+	//		col 1	col 2	col 3
+	// row 1	1	2	3
+	// row 2	4	5	6
+	// row 3	7	8	9
+	// row 4	*	0	#
+
+	//////////OUTPUTS//////////
+	//GPIO10 -- row 1
+	row1 :=  rpio.Pin(10)
+	//GPIO3 -- row 2
+	row2 := rpio.Pin(3)
+	//GPIO4 -- row 3
+	row3 := rpio.Pin(4)
+	//GPIO27 -- row 4
+	row4 := rpio.Pin(27)
+
+	row1.Output()
+	row2.Output()
+	row3.Output()
+	row4.Output()
+
+	//////////INPUTS//////////
+	//GPIO22 -- column 1
+	column1 := rpio.Pin(22)
+	//GPIO9 -- column 2
+	column2 := rpio.Pin(9)
+	//GPIO17 -- column 3
+	column3 := rpio.Pin(17)
+
+	column1.Input()
+	column1.PullUp()
+	column2.Input()
+	column2.PullUp()
+	column3.Input()
+	column3.PullUp()
+
+	//creating arrays of objects pins
+	outputs := [4]rpio.Pin{row1, row2, row3, row4}
+	inputs := [3]rpio.Pin{column1, column2, column3}
+
+	//keyboard that will be used to output what number was pressed
+	keyboardKey := [4][3]string{
+		{"1", "2", "3"},
+		{"4", "5", "6"},
+		{"7", "8", "9"},
+		{"*", "0", "#"},
+	}
+
+	wasPressed := [4][3]bool{
+		{ false, false, false },
+		{ false, false, false },
+		{ false, false, false },
+		{ false, false, false },
+	}
+
+	//variable will be saving the input of the user
+	userPin := ""
+
 	for {
-		fmt.Println("Publishing \"hello\" to \"hello\"")
-
 		if token := client.Publish("hello", 0, false, "This is it"); token.Wait() && token.Error() != nil {
-			fmt.Println(token.Error())
-		}
-
-		//for topic /keyboard when it is used in a lock
-		if token := client.Publish("/bast/csulb-bast/backdoor/keyboard", 0, false, "backdoor keyboard engaged"); token.Wait() && token.Error() != nil {
 			fmt.Println(token.Error())
 		}
 
@@ -48,7 +105,43 @@ func main() {
 			fmt.Println(token.Error())
 		}
 
+		//for loop will loop through the outputs
+		for row := 0; row < 4; row++ {
 
-		time.Sleep(time.Second)
+			//sets the current output to be 0
+			outputs[row].Low()
+
+			//for loop will loop through the inputs
+			for column := 0; column < 3; column++ {
+
+				//checks whether an input is 0
+				if inputs[column].Read() == 0 && !wasPressed[row][column] {
+					wasPressed[row][column] = true
+
+					//if statment will recognize if # was pressed by user
+					if keyboardKey[row][column] == "#" {
+						//breaks out of loop
+						fmt.Println(userPin)
+
+						//for topic /keyboard when it is used in a lock
+						if token := client.Publish("/bast/csulb-bast/backdoor/keypad", 0, false, userPin); token.Wait() && token.Error() != nil {
+							fmt.Println(token.Error())
+						}
+
+						userPin = ""
+						break
+					}
+
+					//saves the keypressed to userPin variable
+					userPin = userPin + keyboardKey[row][column]
+				} else if inputs[column].Read() != 0 {
+					wasPressed[row][column] = false
+				}
+			}
+
+			//closes the connection
+			outputs[row].High()
+		}
 	}
 }
+
